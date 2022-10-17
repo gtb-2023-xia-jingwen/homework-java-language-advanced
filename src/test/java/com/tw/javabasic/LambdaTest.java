9b9d4c08edad549275be7a0d72845db6286c40b5
Jingwen Xia <amyfor7@gmail.com>
2022-10-17 13:07:53 +0800

    [Jingwen Xia] fix: bind lambda

--- src/test/java/com/tw/javabasic/LambdaTest.java
+++ src/test/java/com/tw/javabasic/LambdaTest.java
@@ -32,7 +32,7 @@ class LambdaTest {
         // Hint
         //  For reference please check page 319 of "Core Java Vol 1", section 6.3.4.
         // <--start
-        final StringFunc lambda = null;
+        final StringFunc lambda = () -> "instanceMethod";
         // --end-->
 
         assertEquals("instanceMethod", lambda.getString());
