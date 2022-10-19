db94ac96bb73e95c6c6a3ba7e30c302b0fd71b84
Jingwen Xia <amyfor7@gmai.com>
2022-10-19 22:32:31 +0800

    [Jingwen Xia] fix: calculate sum of each group

--- src/test/java/com/tw/javabasic/StreamingTest.java
+++ src/test/java/com/tw/javabasic/StreamingTest.java
@@ -424,7 +424,11 @@ class StreamingTest {
         // TODO: implement grouping collector using `stream.collect`. You should use `Collectors.groupingBy` and
         // TODO: downstream collector.
         // <--start
-        Map<String, Integer> map = null;
+        Map<String, Integer> map = stream.collect(Collectors.groupingBy(
+                KeyValuePair::getKey,
+                HashMap::new,
+                Collectors.summingInt(KeyValuePair::getValue)
+        ));
         // --end-->
 
         assertEquals(2, map.size());
